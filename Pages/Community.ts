94dd35b48aa10563941a89b47c080aa511aa6591
Siddharth <skumar2_be19@thapar.edu>
2025-09-01 11:40:51 +0530

    harmony

--- Pages/Community.ts
+++ Pages/Community.ts
@@ -2,10 +2,10 @@ import {test,expect,Locator,Page} from "@playwright/test"
 export class Community{
   //Locators
    public communityBlogTitles:Locator 
-   public log:Locator 
+   public harmony:Locator 
     constructor(private page:Page){
       this.communityBlogTitles=this.page.locator(`//div[@id="root"]//following::div[contains(@class,"hmdnzv")]`)
-      this.log=this.page.locator(`  //sample`)
+      this.harmony=this.page.locator(`/sample`)
       
     }
 }
