bc04588acfa8fa33b9dcbafbb16ecbd69371f43b
Siddharth <skumar2_be19@thapar.edu>
2025-09-01 11:12:57 +0530

    community header added

--- Pages/Community.ts
+++ Pages/Community.ts
@@ -2,10 +2,10 @@ import {test,expect,Locator,Page} from "@playwright/test"
 export class Community{
   //Locators
    public communityBlogTitles:Locator 
-   public Blog:Locator 
+   public communityHeader:Locator 
     constructor(private page:Page){
       this.communityBlogTitles=this.page.locator(`//div[@id="root"]//following::div[contains(@class,"hmdnzv")]`)
-      this.Blog=this.page.locator(`//xpath`)
+      this.communityHeader=this.page.locator(``)
       
     }
 }
