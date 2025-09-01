62fc29e77fd01253415be8fd1873f277f8034450
Siddharth <skumar2_be19@thapar.edu>
2025-09-01 11:23:35 +0530

    log

--- Pages/Community.ts
+++ Pages/Community.ts
@@ -2,10 +2,10 @@ import {test,expect,Locator,Page} from "@playwright/test"
 export class Community{
   //Locators
    public communityBlogTitles:Locator 
-   public disCommunityHeader:Locator 
+   public log:Locator 
     constructor(private page:Page){
       this.communityBlogTitles=this.page.locator(`//div[@id="root"]//following::div[contains(@class,"hmdnzv")]`)
-      this.disCommunityHeader=this.page.locator(`  //sample`)
+      this.log=this.page.locator(`  //sample`)
       
     }
 }
