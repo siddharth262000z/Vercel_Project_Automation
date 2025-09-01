ff46535acf3cbf5490708d296cfa1301bb53d46f
Siddharth <skumar2_be19@thapar.edu>
2025-09-01 10:45:24 +0530

    added changes in feature

--- Pages/Community.ts
+++ Pages/Community.ts
@@ -1,8 +1,11 @@
 import {test,expect,Locator,Page} from "@playwright/test"
 export class Community{
   //Locators
-   public communityBlogTitles:Locator  
+   public communityBlogTitles:Locator 
+   public sample:Locator 
     constructor(private page:Page){
       this.communityBlogTitles=this.page.locator(`//div[@id="root"]//following::div[contains(@class,"hmdnzv")]`)
+      this.sample=this.page.locator(``)
+      this.sample=this.page.locator(``)
     }
 }
