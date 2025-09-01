1da9da8db74804b058a51b12eadd10437708e93d
Siddharth <skumar2_be19@thapar.edu>
2025-09-01 11:16:17 +0530

    eg4

--- Pages/Community.ts
+++ Pages/Community.ts
@@ -2,10 +2,10 @@ import {test,expect,Locator,Page} from "@playwright/test"
 export class Community{
   //Locators
    public communityBlogTitles:Locator 
-   public communityHeader:Locator 
+   public disCommunityHeader:Locator 
     constructor(private page:Page){
       this.communityBlogTitles=this.page.locator(`//div[@id="root"]//following::div[contains(@class,"hmdnzv")]`)
-      this.communityHeader=this.page.locator(``)
+      this.disCommunityHeader=this.page.locator(`//sample`)
       
     }
 }
